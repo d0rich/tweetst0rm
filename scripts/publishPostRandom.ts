77d455b631394e60244ea14d1f5abecea1131a28
Nikolay Dorofeev <dorich2000@gmail.com>
2022-08-05 21:40:13 +0400

    Chanse for publishing in dotenv

--- scripts/publishPostRandom.ts
+++ scripts/publishPostRandom.ts
@@ -1,11 +1,13 @@
 import { connect } from '../src/connect/mongodb.connect'
 import { connectTwitter } from '../src/connect/twitter.connect'
 import { publishPost } from '../src/services/publish'
+import 'dotenv/config'
 
 async function main(){
   const luck = Math.random()
+  const chance = Number(process.env.CHANCE)
   console.log(`Luck is ${luck}`)
-  if (luck > (1 - 1/24)){
+  if (luck > (1 - chance)){
     console.log('Post will be published')
     const mongo = await connect()
     const twitterClient = connectTwitter()
