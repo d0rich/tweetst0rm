f2a218f9ad8cb504cf16775f281e2a5e3063a07e
Nikolay Dorofeev <dorich2000@gmail.com>
2022-08-05 21:17:01 +0400

    Fix chances

--- scripts/publishPostRandom.ts
+++ scripts/publishPostRandom.ts
@@ -3,13 +3,18 @@ import { connectTwitter } from '../src/connect/twitter.connect'
 import { publishPost } from '../src/services/publish'
 
 async function main(){
-  if (Math.random() > (1/24)){
+  const luck = Math.random()
+  console.log(`Luck is ${luck}`)
+  if (luck > (1 - 1/24)){
+    console.log('Post will be published')
     const mongo = await connect()
     const twitterClient = connectTwitter()
     const tokensCollection = mongo.db('auth').collection('tokens')
     await publishPost(tokensCollection, twitterClient)
     mongo.close()
   }
+  else
+    console.log('Post won\'t be published')
   
 }
 
